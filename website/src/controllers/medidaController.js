eae97f9780db6e845257d9f7a84827e91c880543
cindybarbosa <cindy.barbosa@sptech.school>
2024-06-10 17:48:45 -0300

    Conexão da 1° DashBoard com o Banco de Dados

--- website/src/controllers/medidaController.js
+++ website/src/controllers/medidaController.js
@@ -1,48 +1,23 @@
 var medidaModel = require("../models/medidaModel");
 
-function buscarUltimasMedidas(req, res) {
+function buscarMedidasDoDia(req, res) {
+    var idUsuario = req.params.idUsuario;
 
-    const limite_linhas = 7;
+    console.log(`Recuperando medidas do dia atual para o usuário ${idUsuario}`);
 
-    var idAquario = req.params.idAquario;
-
-    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
-
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarMedidasDoDia(idUsuario).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
-            res.status(204).send("Nenhum resultado encontrado!")
+            res.status(204).send([]);
         }
     }).catch(function (erro) {
         console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
-}
-
-
-function buscarMedidasEmTempoReal(req, res) {
-
-    var idAquario = req.params.idAquario;
-
-    console.log(`Recuperando medidas em tempo real`);
-
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+        console.log("Houve um erro ao buscar as medidas do dia.", erro.sqlMessage);
+        res.status(500).json({ error: erro.sqlMessage });
     });
 }
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
-
+    buscarMedidasDoDia
 }
--- website/src/models/medidaModel.js
+++ website/src/models/medidaModel.js
@@ -1,35 +1,24 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-function buscarMedidasEmTempoReal(idAquario) {
-
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+function buscarMedidasDoDia(idUsuario) {
+    var instrucaoSql = `
+        SELECT 
+            m.dht11_temperatura, 
+            m.dht11_umidade,
+            DATE_FORMAT(m.dtCaptura, '%H:%i:%s') as dtCaptura
+        FROM medida m
+        JOIN sensores s ON m.fkSensores = s.idSensores
+        JOIN capril c ON s.fkCaprilSensor = c.idCapril
+        JOIN fazenda f ON c.fkFazenda = f.idFazenda
+        WHERE f.idFazenda = ${idUsuario}
+        AND DATE(m.dtCaptura) = CURDATE()
+        ORDER BY m.dtCaptura DESC;
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarMedidasDoDia
 }
