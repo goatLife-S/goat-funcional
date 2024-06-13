53f2a257b5e3f51af191c95aa655936a56b7eb4c
Vitorhugoa <vitor.andrade@sptech.school>
2024-06-13 04:00:05 -0300

    Alertas funcionados de for correta

--- website/public/js/AlertaGoat.js
+++ website/public/js/AlertaGoat.js
@@ -1,59 +1,137 @@
-function AlertaTemperatura(temperaturas, Alerta) {
-  // prgando os valores do banco
-  let TamanhoDoArray = temperaturas.length
+let Ligado = true;
+function AlertaTemperatura(temperaturas, AlertaVisao) {
+  // Pegando os valores do banco
+  let TamanhoDoArray = temperaturas.length;
 
-  const TemperaturaMuitoAlta = 36;
-  const TemperaturaAlta = 35;
+  const TemperaturaMuitoAlta = 35;
+  const TemperaturaAlta = 31.5;
   const TemperaturaBaixa = 20;
-  const TemperaturaMuitoBaixa = 10;
-  
+  const TemperaturaMuitoBaixa = 15;
+  let UltimoValor = 0;
 
-  let Sinal = {
-    MuitoAlta: false,
-    Alta: false,
-    Ideal: false,
-    Baixa: false,
-    MuitoBaixa: false
+  if(Ligado){
+    document.getElementById("AlertaTempera").innerHTML = ``;
+      document.getElementById("AlertaTempera").style.display = "flex";
+  }
+  for (let i = 0; i < TamanhoDoArray; i++) {
+    let posicaoAtual = temperaturas[i];
+    UltimoValor = posicaoAtual;
+  }
+    // Validação para ver se não é nulo o valor 
+    if (UltimoValor === undefined || isNaN(UltimoValor)) {
+      console.log("fala Programador! De boas? Então eu tenho uma noticia ruim, o seu código deu B.O.");
+    } 
+    else if (UltimoValor > TemperaturaMuitoAlta) {
+      console.log("Temperatura sendo captada, mas acima do normal");
+      AlertaVisao.innerHTML += `
+       <div class="AlertaAtualTemperatura1">
+                        <h1>Temperatura do capril muito alta</h1>
+                      <img class="imageTemp" src="./assets/siren.png" alt="">
+                        <button class="btn-alerta" onclick="Fechar()">x</button>
+                    </div>
+      `;
+    } 
+    else if (UltimoValor > TemperaturaAlta) {
+      console.log("Temperatura sendo captada, mas está alta");
+      AlertaVisao.innerHTML += `
+        <div class="AlertaAtualTemperatura2">
+                        <h1>Temperatura do capril alta</h1>
+                      <img class="imageTemp" src="./assets/siren.png" alt="">
+                        <button class="btn-alerta" onclick="Fechar()">x</button>
+
+                    </div>
+      `;
+    }
+     else if (UltimoValor < TemperaturaBaixa && UltimoValor >= TemperaturaMuitoBaixa) {
+      console.log("Temperatura sendo captada, mas está baixa");
+      AlertaVisao.innerHTML += `
+       <div class="AlertaAtualTemperatura4">
+                        <h1>Temperatura do capril baixa</h1>
+                      <img class="imageTemp" src="./assets/siren.png" alt="">
+                        <button class="btn-alerta" onclick="Fechar()">x</button>
+
+                    </div>
+      `;
+    } 
+    else if (UltimoValor < TemperaturaMuitoBaixa) {
+      console.log("Temperatura sendo captada, mas está muito abaixo do esperado");
+      AlertaVisao.innerHTML += ` 
+      <div class="AlertaAtualTemperatura5">
+                        <h1>Temperatura do capril Muito baixa</h1>
+                      <img class="imageTemp" src="./assets/siren.png" alt="">
+                        <button class="btn-alerta" onclick="Fechar()">x</button>
+                    </div>`;
+    }
   }
 
-  for (let i = 0; i <= TamanhoDoArray; i++) {
-    let posicaoAtual = temperaturas[i]
-    //Validação para ver se não e nulo o valor 
-    if (posicaoAtual == undefined || posicaoAtual == NaN) {
-      console.log("fala Programador! De boas? /n Então eu tenho uma noticia ruim, o seu código deu B.O.")
-      
-      
-      } else if (posicaoAtual > TemperaturaMuitoAlta) {
-        console.log("temperatura sendo captada, mas acima do normal")
-        AlertaTempera.innerHTML = `<div class="AlertaAtualTemperatura1"><h1>Temperatura do capril muito alta</div>`
 
-      Sinal.MuitoAlta = true
+  function Fechar() {
+    document.getElementById("AlertaTempera").style.display = "none";
+  }
 
-
-    } else if (posicaoAtual > TemperaturaAlta) {
-      console.log("Temperatura sendo captada, mas está alta")
-      Sinal.Alta = true
-
-
-    } else if (posicaoAtual < TemperaturaAlta && posicaoAtual < TemperaturaBaixa) {
-      console.log("Temperatura sendo captada, e está dentro do limite esperado")
-      Sinal.Ideal = true
-
-
-    } else if (posicaoAtual < TemperaturaBaixa) {
-      console.log("Temperatura sendo captada, mas esta baixa")
-      Sinal.Baixa = true
-
-
-    } else if (posicaoAtual < TemperaturaMuitoBaixa) {
-      console.log("Temperatura sendo captada, mas está muito abaixo do esperado")
-      Sinal.MuitoBaixa = true
-
+  function AlertaUmidade(umidades, AlertaVisao) {
+    // Pegando os valores do banco
+    let TamanhoDoArray = umidades.length;
+  
+    const umidadeMuitoAlta = 35;
+    const umidadeAlta = 31.5;
+    const umidadeBaixa = 20;
+    const umidadeMuitoBaixa = 15;
+    let UltimoValor = 0;
+    for (let i = 0; i < TamanhoDoArray; i++) {
+      let posicaoAtual = umidades[i];
+      UltimoValor = posicaoAtual;
 
     }
+      // Validação para ver se não é nulo o valor 
+      if (UltimoValor === undefined || isNaN(UltimoValor)) {
+        console.log("fala Programador! De boas? Então eu tenho uma noticia ruim, o seu código deu B.O.");
+      } 
+      else if (UltimoValor > umidadeMuitoAlta) {
+        console.log("umidade sendo captada, mas acima do normal");
+        AlertaVisao.innerHTML += `
+         <div class="AlertaAtualumidade1">
+                          <h1>umidade do capril muito alta</h1>
+                          <img class="imageTemp" src="./assets/siren.png" alt="">
+                            <button class="btn-alerta" onclick="Fechar()">x</button>
+                      </div>
+        `;
+      } 
+      else if (UltimoValor > umidadeAlta) {
+        console.log("umidade sendo captada, mas está alta");
+        AlertaVisao.innerHTML += `
+          <div class="AlertaAtualumidade2">
+                          <h1>umidade do capril alta</h1>
+                        <img class="imageTemp" src="./assets/siren.png" alt="">
+                        <button class="btn-alerta" onclick="Fechar()">x</button>
+                          </div>
+        `;
+      }
+       else if (UltimoValor < umidadeBaixa && UltimoValor >= umidadeMuitoBaixa) {
+        console.log("umidade sendo captada, mas está baixa");
+        AlertaVisao.innerHTML += `
+         <div class="AlertaAtualumidade4">
+                          <h1>umidade do capril baixa</h1>
+                        <img class="imageTemp" src="./assets/siren.png" alt="">
+                          <button class="btn-alerta" onclick="Fechar()">x</button>
+                          </div>
+        `;
+      } 
+      else if (UltimoValor < umidadeMuitoBaixa) {
+        console.log("umidade sendo captada, mas está muito abaixo do esperado");
+        AlertaVisao.innerHTML += ` 
+        <div class="AlertaAtualumidade5">
+                          <h1>umidade do capril Muito baixa</h1>
+                          <img class="imageTemp" src="./assets/siren.png" alt="">
+                          <button class="btn-alerta" onclick="Fechar()">x</button>
+                      </div>`;
+      }
+    }
+  
+  
 
-
-  }
-}
-
-
+    function Fechar() {
+      Ligado = false;
+      document.getElementById("AlertaTempera").innerHTML = ``;
+      document.getElementById("AlertaTempera").style.display = "none";
+    }
